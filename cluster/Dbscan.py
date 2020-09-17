2ac3f748dd3f686b7180213cea232b8ca4bb4f04
姚正发 <976691141@qq.com>
2020-09-17 21:36:27 +0800

    close #11 完成代码开发

--- cluster/Dbscan.py
+++ cluster/Dbscan.py
@@ -11,7 +11,7 @@ def simpleTest():
     scaler.fit(x)
     x_scaled = scaler.transform(x)
 
-    dbscan = DBSCAN()
+    dbscan = DBSCAN() # 支持eps与min_samples 参数设置
     clusters = dbscan.fit_predict(x_scaled)
     plt.scatter(x_scaled[:, 0], x_scaled[:, 1], c=clusters, cmap=mglearn.cm2, s=60)
     plt.xlabel("Feature 0")
