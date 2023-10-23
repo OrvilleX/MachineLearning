7d2817c23434026889131c021290d8a1b1c0f457
alone <976691141@qq.com>
2023-10-23 14:51:21 +0800

    增加针对yolo输入图片尺寸对目标检测的注意点

--- cnn/lane/example.py
+++ cnn/lane/example.py
@@ -3,6 +3,10 @@ import numpy as np
 
 
 def hough_line():
+    """
+    Hough_line直线检测算法
+    :return:
+    """
     img = cv2.imread('../../data/lane/lane2.png')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 50, 200, apertureSize=3)
@@ -22,6 +26,10 @@ def hough_line():
 
 
 def hougnp_line():
+    """
+    HoughP_line直线检测算法
+    :return:
+    """
     img = cv2.imread('../../data/lane/lane2.png')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 50, 200, apertureSize=3)
@@ -31,7 +39,48 @@ def hougnp_line():
     cv2.imshow("result", img)
 
 
+def lsd_line():
+    """
+    LSD直线检测算法
+    :return:
+    """
+    img = cv2.imread('../../data/lane/lane2.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    lsd = cv2.createLineSegmentDetector(0)
+    dlines = lsd.detect(gray)
+
+    for dline in dlines[0]:
+        x0 = int(round(dline[0][0]))
+        y0 = int(round(dline[0][1]))
+        x1 = int(round(dline[0][2]))
+        y1 = int(round(dline[0][3]))
+        cv2.line(img, (x0, y0), (x1, y1), (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.imshow("result", img)
+
+
+def fld_line():
+    """
+
+    :return:
+    """
+    img = cv2.imread('../../data/lane/lane2.png')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    fld = cv2.ximgproc.createFastLineDetector()
+    dlines = fld.detect(gray)
+    for dline in dlines:
+        x0 = int(round(dline[0][0]))
+        y0 = int(round(dline[0][1]))
+        x1 = int(round(dline[0][2]))
+        y1 = int(round(dline[0][3]))
+        cv2.line(img, (x0, y0), (x1, y1), (0, 255, 0), 1, cv2.LINE_AA)
+    cv2.imshow("result", img)
+    # drawn_img = fld.drawSegments(img, dlines)
+    # cv2.imshow("result", drawn_img)
+
+
 if __name__ == '__main__':
     # hough_line()
-    hougnp_line()
+    # hougnp_line()
+    # lsd_line()
+    # fld_line()
     cv2.waitKey(0)
--- docs/cnn/LaneDetection.md
+++ docs/cnn/LaneDetection.md
@@ -1,6 +1,18 @@
-# 车道线检测
+# 直线检测
+
+## 
+
+需要安装对应OpenCV的扩展库  
+
+```bash
+pip install opencv-contrib-python
+```
+
+### EDLines算法模型
+
+由于其相对C++的版本更贴合实际的场景，为了我们需要依赖[ED_Lib](https://github.com/CihanTopal/ED_Lib)并通过C++语言进行开发调试。
 
 ## 参考文献
 
-* [直线检测算法汇总](https://mp.weixin.qq.com/s/eOLTnrIPaMoiRneN_CbaIA)  
+* [直线检测算法汇总](https://mp.weixin.qq.com/s/eOLTnrIPaMoiRneN_CbaIA)
 * [车道线检测SOTA模型CLRNet复现](https://aistudio.baidu.com/projectdetail/6724057?channelType=0&channel=0)
